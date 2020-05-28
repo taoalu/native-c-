33dc66f1fd01560fc1eeb2948b24db3ccafb5a34
Thiago <taoalu@gmail.com>
2020-05-28 10:15:57 -0300

    Creation of the application, initial functional version.

---
+++ app/src/main/cpp/Renderer.cpp
@@ -0,0 +1,14 @@
+//
+// Created by taoal on 28/05/2020.
+//
+
+#include "Renderer.h"
+#include "../../../../../../../AppData/Local/Android/Sdk/ndk/20.0.5594570/sources/android/native_app_glue/android_native_app_glue.h"
+
+Renderer::Renderer(android_app *app) {
+
+}
+
+void Renderer::render() {
+
+}
---
+++ app/src/main/cpp/Renderer.h
@@ -0,0 +1,18 @@
+//
+// Created by taoal on 28/05/2020.
+//
+
+#ifndef WARTALEMOBILE_RENDERER_H
+#define WARTALEMOBILE_RENDERER_H
+
+
+class Renderer {
+
+public:
+    Renderer(android_app *app);
+
+    void render();
+};
+
+
+#endif //WARTALEMOBILE_RENDERER_H
